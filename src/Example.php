63aff0bc20a063290ee77de4c4849c494683f5db
辛建华 <xinjh0815@126.com>
2016-08-17 21:14:29 +0800

    初始化项目

---
+++ src/Example.php
@@ -0,0 +1,15 @@
+<?php
+namespace composerTest;
+/**
+ * Created by PhpStorm.
+ * User: xinjianhua
+ * Date: 16/8/17
+ * Time: 20:58
+ */
+class Example
+{
+    public function index()
+    {
+        echo 'This is test!';
+    }
+}
